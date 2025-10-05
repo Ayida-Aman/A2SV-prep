c523088b13b9e1a6e127dba51f856881cccbbebb
Ayida-Aman <amanayida1@gmail.com>
2025-10-05 10:42:58 +0300

    sort colors

---
+++ sorting/merge-sort/sort-colors.js
@@ -0,0 +1,22 @@
+// problem link https://leetcode.com/problems/sort-colors/
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  count = [0, 0, 0];
+  for (let i = 0; i < nums.length; i++) {
+    count[nums[i]]++;
+  }
+
+  let index = 0;
+  for (let i = 0; i < count[0]; i++) {
+    nums[index++] = 0;
+  }
+  for (let i = 0; i < count[1]; i++) {
+    nums[index++] = 1;
+  }
+  for (let i = 0; i < count[2]; i++) {
+    nums[index++] = 2;
+  }
+};
