76cc22eb3b2db6ae665bb7f5e63a801829865bb5
Ayida-Aman <amanayida1@gmail.com>
2025-11-16 03:16:36 +0300

    added an optimized solution

--- sorting2/relativeSortArray.py
+++ sorting2/relativeSortArray.py
@@ -33,3 +33,35 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: List[int]
         """
+
+
+# attempt 2 (use the built in method)
+
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+
+        freq = {}
+        leftOvers = []
+        arr3 = []
+        arr2Set = set(arr2)
+        for num in arr1:
+            if num in arr2Set:
+                if num in freq:
+                    freq[num] +=1
+                else:
+                    freq[num] = 1
+            else:
+                leftOvers.append(num)
+        for num in arr2:
+            while freq[num] > 0:
+                arr3.append(num)
+                freq[num] -=1
+        leftOvers.sort()
+        return arr3 + leftOvers
+
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        
