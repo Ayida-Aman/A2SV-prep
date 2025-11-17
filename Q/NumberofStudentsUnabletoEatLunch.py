5bcf89a507a373c18a2aedf970abd0b2af890910
Ayida-Aman <amanayida1@gmail.com>
2025-11-17 11:09:09 +0300

    number of students unable to eat lunch using Q

---
+++ Q/NumberofStudentsUnabletoEatLunch.py
@@ -0,0 +1,26 @@
+# problem link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/description/
+
+from collections import deque
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        studentsQ = deque(students)
+        sandwichesQ = deque(sandwiches)
+        count =0
+        while studentsQ and sandwichesQ:
+            if studentsQ[0] == sandwichesQ[0]:
+                studentsQ.popleft()
+                sandwichesQ.popleft()
+                count = 0
+            else:
+                studentsQ.append(studentsQ.popleft())
+                count +=1
+            if count == len(studentsQ):
+                break
+        return (count)
+
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+        
